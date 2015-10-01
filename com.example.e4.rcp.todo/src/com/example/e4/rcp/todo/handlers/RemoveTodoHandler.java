c0b440b0013eb8260707f35fea83d142497f0eaa
Sara Gomez Nosti <saragn@gmail.com>
2015-10-01 15:31:27 +0200

    inject selection instead of selection service

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/RemoveTodoHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/RemoveTodoHandler.java
@@ -1,27 +1,27 @@
- 
+
 package com.example.e4.rcp.todo.handlers;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.services.IServiceConstants;
 
 import com.example.e4.rcp.todo.model.ITodoService;
 import com.example.e4.rcp.todo.model.Todo;
 
 public class RemoveTodoHandler {
-	
+
 	@Inject
 	ITodoService model;
-	
-	@Inject
-	ESelectionService selectionService;
-	
+
 	@Execute
-	public void execute() {
+	public void execute(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) Todo todo) {
 		System.out.println(this.getClass().getSimpleName() + " called.");
-		Todo todo = (Todo) selectionService.getSelection();
-		model.deleteTodo(todo.getId());
+		// todo is optional in case the selection is not a Todo object
+		if (todo != null)
+			model.deleteTodo(todo.getId());
 	}
-		
+
 }
