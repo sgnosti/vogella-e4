f1a398b6ac7005bdf4c3c1bb7467856284b2cc0a
Sara Gomez Nosti <saragn@gmail.com>
2015-10-01 15:26:31 +0200

    remove todo handler

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/RemoveTodoHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/RemoveTodoHandler.java
@@ -1,12 +1,27 @@
  
 package com.example.e4.rcp.todo.handlers;
 
+import javax.inject.Inject;
+
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+
+import com.example.e4.rcp.todo.model.ITodoService;
+import com.example.e4.rcp.todo.model.Todo;
 
 public class RemoveTodoHandler {
+	
+	@Inject
+	ITodoService model;
+	
+	@Inject
+	ESelectionService selectionService;
+	
 	@Execute
 	public void execute() {
 		System.out.println(this.getClass().getSimpleName() + " called.");
+		Todo todo = (Todo) selectionService.getSelection();
+		model.deleteTodo(todo.getId());
 	}
 		
 }
