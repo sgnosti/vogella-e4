ef94001e9ffafd0a785ad1c66218f5a9f9e2abf3
Sara Gomez Nosti <saragn@gmail.com>
2015-10-02 11:43:37 +0200

    ex 112 - dynamic part - not complete yet

---
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ValidateHandler.java
@@ -0,0 +1,12 @@
+ 
+package com.example.e4.rcp.todo.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class ValidateHandler {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
